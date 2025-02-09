aab37b5a186104a2368f7602d714fb921aa6c0a8
tuien1 <duongtuyen05042007@gmail.com>
2025-02-09 15:44:33 +0700

    sloo

--- lib/dart_application_basic.dart
+++ lib/dart_application_basic.dart
@@ -1,3 +1,10 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  List<String> names = ['alaa', 'aloo', 'abcde', 'jackbokon'];
+  names.insert(0, 'haiom');
+  /* chen 'haiom' vao vi tri so 0 
+  */
+  names.removeWhere((name) => name.length < 4);
+  // List <String> upperName = name.map((name)=>name.toupperCase()).toList();
+  print("danh sach goc la $names");
+
 }
